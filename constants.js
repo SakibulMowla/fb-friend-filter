efe67ee9493b850352047bced49f447549a0e0be
Sakibul Mowla <sakibulmowla@gmail.com>
2020-04-13 18:34:06 +0100

    Add constants.js file

---
+++ constants.js
@@ -0,0 +1,54 @@
+const defaultInfo = {
+    handle: null,
+    name: null,
+    gender: null,
+    birthDate: null,
+    birthYear: null,
+    language: null,
+    religion: null,
+    currentLocation: null,
+    homeLocation: null,
+    joined: null,
+    mutualFriends: null,
+    followers: null,
+    interestedIn: null,
+    linkedIn: null,
+    googleTalk: null,
+    skype: null,
+    familyMember: [],
+    mobile: []
+};
+
+const familyMemberCategories = [
+    'Cousin',
+    'Brother',
+    'Sister',
+    'Nephew',
+    'Family member',
+    'Grandson',
+    'Brother-in-law'
+];
+
+const profileFeatures = [
+    'about_overview',
+    'about_work_and_education',
+    'about_places',
+    'about_contact_and_basic_info',
+    'about_family_and_relationships',
+    'about_details',
+    'about_life_events'
+];
+
+const titleToExclude = [
+    'About',
+    'Earlier',
+    'Friends',
+    'New',
+    'Notifications',
+    'Messenger'
+];
+
+exports.DefaultInfo = defaultInfo;
+exports.FamilyMemberCategories = familyMemberCategories;
+exports.ProfileFeatures = profileFeatures;
+exports.TitleToExclude = titleToExclude;
